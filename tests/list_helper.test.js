b0ec20a3193616dc8871c819a56d555449a9bf3e
Tero Kilpeläinen <degenone1@gmail.com>
2023-10-23 16:33:31 +0300

    exercise 4.5 done

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -6,61 +6,61 @@ test('dummy returns one', () => {
     expect(result).toBe(1);
 });
 
-describe('total likes', () => {
-    const emptyBlobList = [];
-    const oneBlogList = [
-        {
-            _id: '6536503b7620fa0d2cc5d82e',
-            title: 'My first Blog!',
-            author: 'Tero K.',
-            url: 'https://coolblogs.dev/1',
-            likes: 0,
-            __v: 0,
-        },
-    ];
-    const manyBlogsList = [
-        {
-            _id: '6536503b7620fa0d2cc5d82e',
-            title: 'My first Blog!',
-            author: 'Tero K.',
-            url: 'https://coolblogs.dev/1',
-            likes: 0,
-            __v: 0,
-        },
-        {
-            _id: '6536508d7620fa0d2cc5d831',
-            title: 'My second Blog!',
-            author: 'Tero K.',
-            url: 'https://coolblogs.dev/2',
-            likes: 4,
-            __v: 0,
-        },
-        {
-            _id: '65365557eec0b4d41e3864ab',
-            title: 'My cool Blog',
-            author: 'Tero K.',
-            url: 'https://coolblogs.dev/3',
-            likes: 21,
-            __v: 0,
-        },
-        {
-            _id: '6536577b6ae3ad55f179ff19',
-            title: 'My melancholy Blog',
-            author: 'Tero K.',
-            url: 'https://coolblogs.dev/4',
-            likes: 18,
-            __v: 0,
-        },
-        {
-            _id: '6536580fbb2b5c14f138e386',
-            title: 'My FSO Blog',
-            author: 'Tero K.',
-            url: 'https://coolblogs.dev/5',
-            likes: 26,
-            __v: 0,
-        },
-    ];
+const emptyBlobList = [];
+const oneBlogList = [
+    {
+        _id: '6536503b7620fa0d2cc5d82e',
+        title: 'My first Blog!',
+        author: 'Tero K.',
+        url: 'https://coolblogs.dev/1',
+        likes: 0,
+        __v: 0,
+    },
+];
+const manyBlogsList = [
+    {
+        _id: '6536503b7620fa0d2cc5d82e',
+        title: 'My first Blog!',
+        author: 'Tero A.',
+        url: 'https://coolblogs.dev/1',
+        likes: 0,
+        __v: 0,
+    },
+    {
+        _id: '6536508d7620fa0d2cc5d831',
+        title: 'My second Blog!',
+        author: 'Tero B.',
+        url: 'https://coolblogs.dev/2',
+        likes: 4,
+        __v: 0,
+    },
+    {
+        _id: '65365557eec0b4d41e3864ab',
+        title: 'My cool Blog',
+        author: 'Tero C.',
+        url: 'https://coolblogs.dev/3',
+        likes: 21,
+        __v: 0,
+    },
+    {
+        _id: '6536577b6ae3ad55f179ff19',
+        title: 'My melancholy Blog',
+        author: 'Tero D.',
+        url: 'https://coolblogs.dev/4',
+        likes: 18,
+        __v: 0,
+    },
+    {
+        _id: '6536580fbb2b5c14f138e386',
+        title: 'My FSO Blog',
+        author: 'Tero E.',
+        url: 'https://coolblogs.dev/5',
+        likes: 26,
+        __v: 0,
+    },
+];
 
+describe('total likes', () => {
     test('should return 0 with an empty blog list', () => {
         expect(listHelper.totalLikes(emptyBlobList)).toBe(0);
     });
@@ -71,5 +71,27 @@ describe('total likes', () => {
 
     test('should sum likes correctly in list of many blogs', () => {
         expect(listHelper.totalLikes(manyBlogsList)).toBe(69);
+    });
+});
+
+describe('favorite blog', () => {
+    test('should return empty object with empty blog list', () => {
+        expect(listHelper.favoriteBlog(emptyBlobList)).toEqual({});
+    });
+
+    test('should return the one blog from one blog list', () => {
+        expect(listHelper.favoriteBlog(oneBlogList)).toEqual({
+            title: 'My first Blog!',
+            author: 'Tero K.',
+            likes: 0,
+        });
+    });
+
+    test('should correctly pick highest liked blog from many blogs', () => {
+        expect(listHelper.favoriteBlog(manyBlogsList)).toEqual({
+            title: 'My FSO Blog',
+            author: 'Tero E.',
+            likes: 26,
+        });
     });
 });
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,7 +2,19 @@ const dummy = (blogs) => 1;
 
 const totalLikes = (blogs) => blogs.reduce((sum, item) => sum + item.likes, 0);
 
+const favoriteBlog = (blogs) =>
+    blogs.reduce((fav, item) => {
+        return item.likes <= (fav.likes ?? -1)
+            ? fav
+            : {
+                  title: item.title,
+                  author: item.author,
+                  likes: item.likes,
+              };
+    }, {});
+
 module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog,
 };
